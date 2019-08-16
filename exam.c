2b5cf72d0ce243df686804f454b9f198260b447d
gouthamnethi <32320474+gouthamnethi@users.noreply.github.com>
2019-08-16 19:17:43 +0530

    Update exam.c

--- exam.c
+++ exam.c
@@ -2,14 +2,14 @@
 int main() //Main Program
 {
 int num,dis; //Declaring the variables
-scanf("%d %d",&num,&dis);
-int sum=0;
-int p=100-dis;
-while(num>0)
+scanf("%d %d",&num,&dis); //Reading the input from user
+int sum=0; //Declaring and initilazing the sum variable
+int p=100-dis; //Declaring and initilazing the value of variable p with 100-discount
+while(num>0) // While Loop while num variable should be greater than zero 
 {
-    sum+=num;
-    num=(num*p)/100;
+    sum+=num; //sum=sum+num; we are adding the value of the num
+    num=(num*p)/100; // we are decreasing the value of the num with p percentage i.e. num*p% = num*p/100;
 }
-printf("%d",sum);
-return 0;
-}
+printf("%d",sum); //printing the value of the sum
+return 0; //returning the value to the operation system
+} //Ending of the main program
